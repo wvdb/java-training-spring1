259f474ef7a36aab4190881a3aa119d5f7145ea5
wvdbrande <wim.vandenbrande@one-agency.be>
2017-08-25 14:03:25 +0200

    first commit

---
+++ src/test/java/be/ictdynamic/training/spring1/Spring1ApplicationTests.java
@@ -0,0 +1,16 @@
+package be.ictdynamic.training.spring1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
